e30f4867456e57b94b1f1d69ec05c9957893d462
Fernandodg97 <ferdiaz1997@gmail.com>
2025-03-13 20:39:05 +0100

    feat: planteamiento inicial

--- src/components/Tablero.tsx
+++ src/components/Tablero.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Casilla from "./Casilla";
 import "./Tablero.css";
 
@@ -6,22 +5,15 @@ function Tablero(){
     const filas = 6;
     const columnas = 6;
     const tamaño = filas * columnas;
-    const [grid,setGrid] = useState<String[][]>( () => {
-        const newGrid:String[][] = Array(6).fill(null).map(() => Array(6).fill(null));
-        newGrid[0][0] = "emoji";
-        return newGrid;
-    }) 
-
     
     return(
         <>
         <div className="tablero">
-            {grid.map((i, index) => (
-                <div key={index} >
-                       {i.map((j,indexJ) => (
-                        <Casilla index={indexJ} emoji=''/>
-                       )
-                    )}
+            {Array.from({length: tamaño }).map((_, index) => (
+                <div key={index} className="celdas" >
+                        {
+                                <Casilla index={index} emoji={ index === 1 ? '🐥' : index === 35? 'huevo' : ''}/>
+                        }
                 </div>
             ))}
         </div>
