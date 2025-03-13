0c1197786c1a961eb52f0c5d35606f01a8c5f73a
david <tlaw4444@gmail.com>
2025-03-13 21:57:13 +0100

    Fix Bidimensional array

--- src/components/Tablero.tsx
+++ src/components/Tablero.tsx
@@ -1,24 +1,30 @@
 import Casilla from "./Casilla";
 import "./Tablero.css";
 
-function Tablero(){
+function Tablero() {
     const filas = 6;
     const columnas = 6;
-    const tamaño = filas * columnas;
-    
-    return(
+
+    const grid = Array.from({ length: filas }, () =>
+        Array.from({ length: columnas }, () => "")
+    );
+
+    grid[0][0] = "🐥"; 
+    grid[5][5] = "🥚"; 
+
+    return (
         <>
-        <div className="tablero">
-            {Array.from({length: tamaño }).map((_, index) => (
-                <div key={index} className="celdas" >
-                        {
-                                <Casilla index={index} emoji={ index === 1 ? '🐥' : index === 35? 'huevo' : ''}/>
-                        }
-                </div>
-            ))}
-        </div>
+            <div className="tablero">
+            {grid.map((row, rowIndex) => (
+                    <div key={rowIndex} className="fila">
+                        {row.map((cell, colIndex) => (
+                            <Casilla index={colIndex} emoji={cell}/>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </>
-    )
+    );
 }
 
 export default Tablero;
